4171651585c9c7f7c33c3075d3f23755d19e4c8d
Natacha Romano <natacharomanonr@gmail.com>
2019-10-14 21:34:47 -0300

    installing express

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const http = require('http');
+const app = require('./src/config/custom-express');
 
-const server = http.createServer(function (req, resp) {
-    resp.end(
-            `
+app.listen(3000, function() {
+    console.log(`Server running on port: 3000`);   
+});
+
+app.get('/', function(req, resp) {
+    resp.send(
+        `
             <html>
                 <head>
                     <meta charset="utf-8">
@@ -15,4 +19,50 @@ const server = http.createServer(function (req, resp) {
     );
 });
 
-server.listen(3000);
+app.get('/livros', function(req, resp) {
+    resp.send(
+        `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Lista de livros </h1>
+                </body> 
+            </html>
+        `
+    );
+});
+// const http = require('http');
+
+// const server = http.createServer(function (req, resp) {
+
+//     let html = '';
+
+//     if (req.url == '/') {
+//         html =  `
+//             <html>
+//                 <head>
+//                     <meta charset="utf-8">
+//                 </head>
+//                 <body>
+//                     <h1> Casa do Código </h1>
+//                 </body> 
+//             </html>
+//         `;
+//     } else if (req.url == '/livros') {
+//         html = `
+//             <html>
+//                 <head>
+//                     <meta charset="utf-8">
+//                 </head>
+//                 <body>
+//                     <h1> Lista de Livros </h1>
+//                 </body> 
+//             </html>
+//         `;
+//     }
+//     resp.end(html);
+// });
+
+// server.listen(3000);
