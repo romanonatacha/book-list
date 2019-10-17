28b8821dc565efb870b5473a2277474d1f4d5a80
Natacha Romano <natacharomanonr@gmail.com>
2019-10-17 15:52:31 -0300

    post method on form

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -31,4 +31,19 @@ module.exports = (app) => {
             .catch(erro => console.log(erro));
 
     });
+
+    app.get('/livros/form', function(req, resp) {
+        resp.marko(require('../views/books/form/form.marko'));
+    });
+
+    app.post('/livros', function(req, resp) {
+        console.log(req.body);
+
+        const livroDao = new LivroDao(db);
+
+        livroDao.adiciona(req.body)
+            .then(resp.redirect('/livros'))
+            .catch(erro => console.log(erro));
+
+    });
 };
