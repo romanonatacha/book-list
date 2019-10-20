5dbef91af8e8527b82cbf6bb4ceeaa1c5626899d
Natacha Romano <natacharomanonr@gmail.com>
2019-10-20 19:21:01 -0300

    remove and update

--- src/app/infra/livro-dao.js
+++ src/app/infra/livro-dao.js
@@ -4,29 +4,15 @@ class LivroDao {
         this._db = db;
     }
 
-    lista() {
-        return new Promise((resolve, reject) => {
-            this._db.all(
-                'SELECT * FROM livros',
-                (erro, resultados) => {
-                    if (erro) return reject('Não foi possível listar os livros!');
-
-                    return resolve(resultados);
-                }
-            )
-
-        });
-    }
-
     adiciona(livro) {
         return new Promise((resolve, reject) => {
             this._db.run(`
-                INSERT INTO LIVROS (
-                    titulo,
+                INSERT INTO livros (
+                    titulo, 
                     preco,
                     descricao
-                ) values (?, ?, ?)
-            `,
+                ) values (?,?,?)
+                `,
                 [
                     livro.titulo,
                     livro.preco,
@@ -41,6 +27,84 @@ class LivroDao {
                     resolve();
                 }
             )
+        });
+    }
+
+    lista() {
+        return new Promise((resolve, reject) => {
+            this._db.all(
+                'SELECT * FROM livros',
+                (erro, resultados) => {
+                    if (erro) return reject('Não foi possível listar os livros!');
+
+                    return resolve(resultados);
+                }
+            )
+        });
+    }
+
+    buscaPorId(id) {
+
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                    SELECT *
+                    FROM livros
+                    WHERE id = ?
+                `,
+                [id],
+                (erro, livro) => {
+                    if (erro) {
+                        return reject('Não foi possível encontrar o livro!');
+                    }
+                    return resolve(livro);
+                }
+            );
+        });
+    }
+
+    atualiza(livro) {
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                UPDATE livros SET
+                titulo = ?,
+                preco = ?,
+                descricao = ?
+                WHERE id = ?
+            `,
+            [
+                livro.titulo,
+                livro.preco,
+                livro.descricao,
+                livro.id
+            ],
+            erro => {
+                if (erro) {
+                    return reject('Não foi possível atualizar o livro!');
+                }
+
+                resolve();
+            });
+        });
+    }
+
+    remove(id) {
+
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                    DELETE 
+                    FROM livros
+                    WHERE id = ?
+                `,
+                [id],
+                (erro) => {
+                    if (erro) {
+                        return reject('Não foi possível remover o livro!');
+                    }
+                    return resolve();
+                }
+            );
         });
     }
 }
--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -33,7 +33,22 @@ module.exports = (app) => {
     });
 
     app.get('/livros/form', function(req, resp) {
-        resp.marko(require('../views/books/form/form.marko'));
+        resp.marko(require('../views/books/form/form.marko'), { livro: {} });
+    });
+
+    app.get('/livros/form/:id', function(req, resp) {
+        const id = req.params.id;
+        const livroDao = new LivroDao(db);
+    
+        livroDao.buscaPorId(id)
+            .then(livro => 
+                resp.marko(
+                    require('../views/books/form/form.marko'),
+                    { livro: livro }
+                )
+            )
+            .catch(erro => console.log(erro));
+    
     });
 
     app.post('/livros', function(req, resp) {
@@ -42,6 +57,17 @@ module.exports = (app) => {
         const livroDao = new LivroDao(db);
 
         livroDao.adiciona(req.body)
+            .then(resp.redirect('/livros'))
+            .catch(erro => console.log(erro));
+
+    });
+
+    app.put('/livros', function(req, resp) {
+        console.log(req.body);
+
+        const livroDao = new LivroDao(db);
+
+        livroDao.atualiza(req.body)
             .then(resp.redirect('/livros'))
             .catch(erro => console.log(erro));
 
